99d37e25354faf4ab87620adffd5b250e7af3d64
Nico Weichbrodt <nico@weichbrodt.me>
2018-03-03 00:40:16 +0100

    First working version, only DPT 9 so far

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 CFLAGS += -std=c11 -O0 -g
+LDFLAGS += -lcurl -lm
 
 knx2influx: knx2influx.o cJSON.o
--- knx2influx.c
+++ knx2influx.c
@@ -12,6 +12,8 @@
 #include <stdint.h>
 #include <string.h>
 #include <errno.h>
+#include <math.h>
+#include <float.h>
 
 #include <curl/curl.h>
 
@@ -25,6 +27,28 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
+typedef struct __ga
+{
+	struct __ga *next;
+	address_t addr;
+	address_t *ignored_senders;
+	size_t ignored_senders_len;
+	char *series;
+	uint8_t dpt;
+	char **tags;
+	size_t tags_len;
+} ga_t;
+
+typedef struct __config
+{
+	char *host;
+	char *database;
+	char *user;
+	char *password;
+	ga_t *gas;
+} config_t;
+
+static config_t config;
 static int socket_fd;
 static struct ip_mreq command = {};
 
@@ -36,6 +60,38 @@ void exithandler()
 		perror("setsockopt (IP_DROP_MEMBERSHIP): ");
 	}
 	close(socket_fd);
+}
+
+void post(char const *data)
+{
+	CURLcode ret;
+	CURL *hnd;
+
+	hnd = curl_easy_init();
+	char host[1024];
+	host[0] = 0;
+	strcat(host, config.host);
+	strcat(host, "/write?db=");
+	strcat(host, config.database);
+	curl_easy_setopt(hnd, CURLOPT_URL, host);
+	curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
+	curl_easy_setopt(hnd, CURLOPT_HEADER, 1L);
+	curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, data);
+	curl_easy_setopt(hnd, CURLOPT_POSTFIELDSIZE_LARGE, (curl_off_t)strlen(data));
+	curl_easy_setopt(hnd, CURLOPT_USERAGENT, "knx2influx");
+	curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 50L);
+	curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_easy_setopt(hnd, CURLOPT_TCP_KEEPALIVE, 0L);
+
+	ret = curl_easy_perform(hnd);
+	curl_easy_cleanup(hnd);
+}
+
+float data_to_2byte_float(uint8_t *data)
+{
+	uint8_t expo = (data[1] & 0b01111000) >> 3;
+	int16_t mant = ((data[1] & 0b10000111) << 8) | data[2];
+	return 0.01f * mant * pow(2, expo);
 }
 
 void process_packet(uint8_t *buf, size_t len)
@@ -83,15 +139,24 @@ void process_packet(uint8_t *buf, size_t len)
 
   // Call callbacks
 	//for (int i = 0; i < registered_callback_assignments; ++i)
+	ga_t *cur = config.gas;
+	while(cur != NULL)
 	{
 		//printf("Testing: 0x%02x%02x\n", callback_assignments[i].address.bytes.high, callback_assignments[i].address.bytes.low);
+		printf("Testing 0x%02x%02x (%u/%u/%u)\n", cur->addr.bytes.high, cur->addr.bytes.low, cur->addr.ga.area, cur->addr.ga.line, cur->addr.ga.member);
 		//if (cemi_data->destination.value == callback_assignments[i].address.value)
+		if (cur->addr.value == cemi_data->destination.value)
 	    {
 			printf("Found match\n");
 			//if (callbacks[callback_assignments[i].callback_id].cond && !callbacks[callback_assignments[i].callback_id].cond())
+			for (int i = 0; i < cur->ignored_senders_len; ++i)
 			{
-        		printf("But it's disabled\n");
-				//continue;
+				address_t a_cur = cur->ignored_senders[i];
+				if (a_cur.value == cemi_data->source.value)
+				{
+					printf("But it's disabled\n");
+					goto next;
+				}
 			}
 			uint8_t data[cemi_data->data_len];
 			memcpy(data, cemi_data->data, cemi_data->data_len);
@@ -101,21 +166,37 @@ void process_packet(uint8_t *buf, size_t len)
 			msg.received_on = cemi_data->destination;
 			msg.data_len = cemi_data->data_len;
 			msg.data = data;
-			//callbacks[callback_assignments[i].callback_id].fkt(msg, callbacks[callback_assignments[i].callback_id].arg);
-			//continue;
+
+			char _post[1024];
+			memset(_post, 0, 1024);
+			strcat(_post, cur->series);
+			for (int i = 0; i < cur->tags_len; ++i)
+			{
+				strcat(_post, ",");
+				strcat(_post, cur->tags[i]);
+			}
+			strcat(_post, " value=");
+			
+			switch (cur->dpt)
+			{
+				case 9:
+				{
+					float val = data_to_2byte_float(data);
+					char buf[3 + DBL_MANT_DIG - DBL_MIN_EXP + 1];
+					snprintf(buf, 3 + DBL_MANT_DIG - DBL_MIN_EXP + 1, "%f", val);
+					strcat(_post, buf);
+				}
+			}
+
+			printf("%s\n", _post);
+			post(_post);
 		}
+next:
+		cur = cur->next;
 	}
+	printf("\n");
 }
 
-typedef struct __config
-{
-	char *host;
-	char *database;
-	char *user;
-	char *password;
-} config_t;
-
-config_t config;
 
 int parse_config()
 {
@@ -146,7 +227,7 @@ int parse_config()
 	}
 	else
 	{
-		error_ptr = "No host given in config!\n";
+		error_ptr = "No host given in config!";
 		goto error;
 	}
 
@@ -157,7 +238,7 @@ int parse_config()
 	}
 	else
 	{
-		error_ptr = "No database given in config!\n";
+		error_ptr = "No database given in config!";
 		goto error;
 	}
 	cJSON *user = cJSON_GetObjectItemCaseSensitive(json, "user");
@@ -171,6 +252,133 @@ int parse_config()
 		config.password = strdup(password->valuestring);
 	}
 
+	// GAs
+	
+	cJSON *gas = cJSON_GetObjectItemCaseSensitive(json, "gas");
+	if (!cJSON_IsArray(gas))
+	{
+		error_ptr = "Expected array, got something else for 'gas'";
+		goto error;
+	}
+	cJSON *ga_obj = NULL;
+
+	ga_t *prev_ga = NULL;
+
+	cJSON_ArrayForEach(ga_obj, gas)
+	{
+		if (!cJSON_IsObject(ga_obj))
+		{
+			error_ptr = "Expected array of ojects, got something that is not object in 'gas'";
+			goto error;
+		}
+		cJSON *ga = cJSON_GetObjectItemCaseSensitive(ga_obj, "ga");
+		if (!cJSON_IsString(ga))
+		{
+			error_ptr = "'ga' is not a string!";
+			goto error;
+		}
+		if (ga->valuestring == NULL)
+		{
+			error_ptr = "'ga' must not be empty!";
+			goto error;
+		}
+		printf("GA: %s", ga->valuestring);
+
+		uint32_t area, line, member;
+		
+		sscanf(ga->valuestring, "%u/%u/%u", &area, &line, &member);
+
+		printf(" -> %u/%u/%u\n", area, line, member);
+
+		cJSON *series = cJSON_GetObjectItemCaseSensitive(ga_obj, "series");
+		if (!cJSON_IsString(series))
+		{
+			error_ptr = "'series' is not a string!";
+			goto error;
+		}
+		if (ga->valuestring == NULL)
+		{
+			error_ptr = "'series' must not be empty!";
+			goto error;
+		}
+		printf("Series: %s\n", series->valuestring);
+
+		cJSON *dpt = cJSON_GetObjectItemCaseSensitive(ga_obj, "dpt");
+		if (!cJSON_IsNumber(dpt))
+		{
+			error_ptr = "'dpt' is not a number!";
+			goto error;
+		}
+
+		ga_t *_ga = calloc(1, sizeof(ga_t));
+
+		if (prev_ga)
+			prev_ga->next = _ga;
+		else
+			config.gas = _ga;
+
+		address_t ga_addr = {.ga={line, area, member}};
+		_ga->addr = ga_addr;
+		_ga->dpt = (uint8_t)dpt->valueint;
+		_ga->series = strdup(series->valuestring);
+
+		cJSON *ignored_senders = cJSON_GetObjectItemCaseSensitive(ga_obj, "ignored_senders");
+		if (!cJSON_IsArray(ignored_senders))
+		{
+			error_ptr = "'ignored_senders' is not an array!";
+			goto error;
+		}
+		_ga->ignored_senders_len = cJSON_GetArraySize(ignored_senders);
+		_ga->ignored_senders = calloc(_ga->ignored_senders_len, sizeof(address_t));
+		int i = 0;
+		cJSON *ignored_sender = NULL;
+		cJSON_ArrayForEach(ignored_sender, ignored_senders)
+		{
+			if (!cJSON_IsString(ignored_sender))
+			{
+				error_ptr = "Expected array of strings, got something that is not a string in 'ignored_senders'";
+				goto error;
+			}
+			if (ignored_sender->valuestring == NULL)
+			{
+				error_ptr = "Got empty string instead of a physical address in 'ignored_senders'";
+				goto error;
+			}
+			uint32_t area, line, member;
+			sscanf(ignored_sender->valuestring, "%u.%u.%u", &area, &line, &member);
+			_ga->ignored_senders[i].pa.area = area;
+			_ga->ignored_senders[i].pa.line = line;
+			_ga->ignored_senders[i].pa.member = member;
+			++i;
+		}
+
+		cJSON *tags = cJSON_GetObjectItemCaseSensitive(ga_obj, "tags");
+		if (!cJSON_IsArray(tags))
+		{
+			error_ptr = "'tags' is not an array!";
+			goto error;
+		}
+		_ga->tags_len = cJSON_GetArraySize(tags);
+		_ga->tags = calloc(_ga->tags_len, sizeof(char *));
+		i = 0;
+		cJSON *tag = NULL;
+		cJSON_ArrayForEach(tag, tags)
+		{
+			if (!cJSON_IsString(tag))
+			{
+				error_ptr = "Expected array of string, got something that is not a string in 'tags'";
+				goto error;
+			}
+			if (tag->valuestring == NULL)
+			{
+				error_ptr = "Got empty string instead of a key=value pair in 'tags'";
+				goto error;
+			}
+			_ga->tags[i] = strdup(tag->valuestring);
+			++i;
+		}
+	}
+	
 	goto end;
 
 error:
